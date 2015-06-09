d799a73b81bed30bdb8697a00f2f9bce71197050
xzv <xzv@a.com>
2015-06-08 17:33:51 -0700

    Fix calculation of velocity after collision
    Delay update of velocity, so when the collide function is called for the
    other party in the collide part, it will not use the new velocity for
    calculation.

--- source/scene/balls.d
+++ source/scene/balls.d
@@ -7,10 +7,15 @@ immutable float[4] dy = [1, -1, -1, 1];
 immutable int[6] quad_index = [0, 1, 2, 0, 2, 3];
 class Ball(int n, int m) : Particle!(n, m) {
 	private {
-		vec2f center, velocity;
+		vec2f center, velocity, newv;
 		float r, av, angle;
+		bool need_update;
 	}
 	override @nogc void update() {
+		if (need_update) {
+			need_update = false;
+			velocity = newv;
+		}
 		center += velocity;
 		angle += av;
 	}
@@ -42,10 +47,16 @@ class Ball(int n, int m) : Particle!(n, m) {
 		import std.conv;
 		assert(dist<20, to!string(dist));
 		float vm = line.dot(velocity)/dist, vo = line.dot(b.velocity)/dist;
-		auto rvm = velocity-(vm*line/dist);
-		velocity = rvm+(vo*line/dist);
-		import std.stdio;
-		writefln("Ball at %s,%s collide with %s,%s", center.x, center.y, b.center.x, b.center.y);
+		//import std.stdio;
+		//writefln("Ball at %s,%s collide with %s,%s, dist %s", center.x, center.y, b.center.x, b.center.y, dist);
+		if (vo-vm > 0) {
+			//writeln("Coming apart");
+			//Don't count as collide if two balls are coming apart
+			return;
+		}
+		auto rvm = (need_update ? newv : velocity)-(vm*line/dist);
+		newv = rvm+(vo*line/dist);
+		need_update = true;
 	}
 	this(vec2f c, vec2f v, float ir, float iav = 0, float a = 0) {
 		center = c;
@@ -53,5 +64,6 @@ class Ball(int n, int m) : Particle!(n, m) {
 		r = ir;
 		angle = a;
 		av = iav;
+		need_update = false;
 	}
 }
